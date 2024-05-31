a2823420a5bce9625e42c86d2d97eb4bbbc5aaff
parthiv <parthivchovatiya0408@gmail.com>
2024-05-31 22:06:10 +0530

    Operators

--- src/pages/Conditional.tsx
+++ src/pages/Conditional.tsx
@@ -29,19 +29,6 @@ console.log("hello",a<18?"not":"Yes")
 //     console.log(`Sorry, we are out of ${expr}.`);
 // }
 
-function greet() {
-    console.log("Hello World!");
-}
-
-let displayPI = function() {
-    console.log("PI = 3.14");
-}
-
-
-greet();
-
-
-displayPI();
 
   return (
     <div>
